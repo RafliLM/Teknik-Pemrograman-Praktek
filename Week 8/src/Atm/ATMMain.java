54ef940e47c102d9da592e6d2fbe9f3b2c74142b
RafliLM <78577283+RafliLM@users.noreply.github.com>
2021-07-10 18:52:36 +0700

    Upload Program ATM

---
+++ Week 8/src/Atm/ATMMain.java
@@ -0,0 +1,12 @@
+package Atm;
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
